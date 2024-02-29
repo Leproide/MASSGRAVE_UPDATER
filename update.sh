c6f3c4aedecdcc070efc2a4b14a1f61618131f85
Mauro <leproide@paranoici.org>
2024-02-29 21:45:40 +0100

    Fixed download URL
    Now the massgrave script name have a crc inside for each version.
    Fixed with the $DownloadURL variable content inside https://massgrave.dev/get.ps1

--- update.sh
+++ update.sh
@@ -5,23 +5,36 @@
 
 path="/var/www/html/changeme"
 
-wget_result="$(wget -NS https://raw.githubusercontent.com/massgravel/Microsoft-Activation-Scripts/master/MAS/All-In-One-Version/MAS_AIO.cmd -P "$path" -O MAS_AIO.cmd_update 2>&1 | grep "HTTP/" | awk '{print $2}')"
+# URL dello script PowerShell
+ScriptURL='https://massgrave.dev/get.ps1'
 
-if [ $wget_result = 200 ]; then
-if cmp -s $path/MAS_AIO.cmd $path/MAS_AIO.cmd_update
-then echo "Files are the same"
-rm $path/MAS_AIO.cmd_update
-echo No update available $(date) >> $path/update.log
-exit 0
-else echo "Files are different"
-rm $path/MAS_AIO.cmd.bak
-mv $path/MAS_AIO.cmd $path/MAS_AIO.cmd.bak
-mv $path/MAS_AIO.cmd_update $path/MAS_AIO.cmd
-echo Updated $(date) >> $path/update.log
-exit 0
-fi
-else
-    echo "Something went wrong"
-        echo DOWNLOAD ERROR - $wget_result - $(date) >> $path/update.log
+# Scarica lo script PowerShell e cerca la variabile $DownloadURL
+DownloadURL=$(curl -s "$ScriptURL" | grep -oP '\$DownloadURL = \x27\K[^\x27]+')
+
+# Controlla se l'estrazione è riuscita
+if [ -n "$DownloadURL" ]; then
+    wget_result="$(wget -NS "$DownloadURL" -P "$path" -O MAS_AIO.cmd_update 2>&1 | grep "HTTP/" | awk '{print $2}')"
+
+    if [ $wget_result = 200 ]; then
+        if cmp -s "$path/MAS_AIO.cmd" "$path/MAS_AIO.cmd_update"; then
+            echo "Files are the same"
+            rm "$path/MAS_AIO.cmd_update"
+            echo "No update available $(date)" >> "$path/update.log"
+            exit 1
+        else
+            echo "Files are different"
+            rm "$path/MAS_AIO.cmd.bak"
+            mv "$path/MAS_AIO.cmd" "$path/MAS_AIO.cmd.bak"
+            mv "$path/MAS_AIO.cmd_update" "$path/MAS_AIO.cmd"
+            echo "Updated $(date)" >> "$path/update.log"
+            exit 1
+        fi
+    else
+        echo "Something went wrong"
+        echo "DOWNLOAD ERROR - $wget_result - $(date)" >> "$path/update.log"
         exit 1
+    fi
+else
+    echo "Impossibile recuperare URL download" >> "$path/update.log"
+    exit 1
 fi
