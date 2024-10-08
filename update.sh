8ce7f0dcae862ffd8dcd8600d754998527107c18
Mauro <leproide@paranoici.org>
2024-10-08 13:44:19 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -6,36 +6,47 @@
 path="/var/www/html/changeme"
 
 # MASSGRAVE GET SCRIPT URL
-ScriptURL='https://massgrave.dev/get.ps1'
+ScriptURL='https://get.activated.win'
 
-# SET $DownloadURL WITH MASSGRAVE WEBSITE PS1 VARIABLE
-DownloadURL=$(curl -s "$ScriptURL" | grep -oP '\$DownloadURL = \x27\K[^\x27]+')
+# CHECK IF SCRIPT URL IS ACCESSIBLE
+if curl --output /dev/null --silent --head --fail "$ScriptURL"; then
+    # Fetching download URLs from the new script
+    DownloadURLs=$(curl -s "$ScriptURL" | grep -oP '(?<=\$DownloadURL[1-3] = \')[^\']+')
 
-# CHECK IF VARIABLE IS CORRECTLY POPULATED
-if [ -n "$DownloadURL" ]; then
-# UPDATING THE LOCAL SCRIPT
-    wget_result="$(wget -NS "$DownloadURL" -P "$path" -O MAS_AIO.cmd_update 2>&1 | grep "HTTP/" | awk '{print $2}')"
+    # Check if we successfully fetched the URLs
+    if [ -n "$DownloadURLs" ]; then
+        # Update each URL into an array
+        IFS=$'\n' read -r -d '' -a urls <<< "$DownloadURLs"
 
-    if [ $wget_result = 200 ]; then
-        if cmp -s "$path/MAS_AIO.cmd" "$path/MAS_AIO.cmd_update"; then
-            echo "Files are the same"
-            rm "$path/MAS_AIO.cmd_update"
-            echo "No update available $(date)" >> "$path/update.log"
-            exit 1
-        else
-            echo "Files are different"
-            rm "$path/MAS_AIO.cmd.bak"
-            mv "$path/MAS_AIO.cmd" "$path/MAS_AIO.cmd.bak"
-            mv "$path/MAS_AIO.cmd_update" "$path/MAS_AIO.cmd"
-            echo "Updated $(date)" >> "$path/update.log"
-            exit 1
-        fi
+        for url in "${urls[@]}"; do
+            # Attempting to download the script
+            wget_result="$(wget -NS "$url" -P "$path" -O MAS_AIO.cmd_update 2>&1 | grep "HTTP/" | awk '{print $2}')"
+
+            if [ $wget_result = 200 ]; then
+                if cmp -s "$path/MAS_AIO.cmd" "$path/MAS_AIO.cmd_update"; then
+                    echo "Files are the same"
+                    rm "$path/MAS_AIO.cmd_update"
+                    echo "No update available $(date)" >> "$path/update.log"
+                    exit 1
+                else
+                    echo "Files are different"
+                    rm "$path/MAS_AIO.cmd.bak" 2>/dev/null
+                    mv "$path/MAS_AIO.cmd" "$path/MAS_AIO.cmd.bak"
+                    mv "$path/MAS_AIO.cmd_update" "$path/MAS_AIO.cmd"
+                    echo "Updated $(date)" >> "$path/update.log"
+                    exit 1
+                fi
+            else
+                echo "Failed to download from $url, HTTP status: $wget_result"
+            fi
+        done
+        echo "All URLs have been tried, no successful download."
+        exit 1
     else
-        echo "Something went wrong"
-        echo "DOWNLOAD ERROR - $wget_result - $(date)" >> "$path/update.log"
+        echo "Cant fetch download url from remote script" >> "$path/update.log"
         exit 1
     fi
 else
-    echo "Cant fetch download url from remote script" >> "$path/update.log"
+    echo "Error: Unable to reach $ScriptURL" >> "$path/update.log"
     exit 1
 fi
