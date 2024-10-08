6774735b0e8319e0f68832c71345bdcbe2f11f2e
Mauro <leproide@paranoici.org>
2024-10-08 13:51:12 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -6,44 +6,46 @@
 path="/var/www/html/changeme"
 
 # MASSGRAVE GET SCRIPT URL
-ScriptURL='https://get.activated.win'
+ScriptURL='https://get.activated.win/get.ps1'
 
 # CHECK IF SCRIPT URL IS ACCESSIBLE
 if curl --output /dev/null --silent --head --fail "$ScriptURL"; then
     # Fetching download URLs from the new script
-    DownloadURLs=$(curl -s "$ScriptURL" | grep -oP '(?<=\$DownloadURL[1-3] = \')[^\']+')
+    DownloadURLs=$(curl -s "$ScriptURL" | grep -oP '\$DownloadURL[1-3] = \x27\K[^\x27]+')
 
     # Check if we successfully fetched the URLs
     if [ -n "$DownloadURLs" ]; then
-        # Update each URL into an array
-        IFS=$'\n' read -r -d '' -a urls <<< "$DownloadURLs"
+        # Convert DownloadURLs to array manually
+        urls=$(echo $DownloadURLs | tr ' ' '\n')  # Use newline as delimiter
 
-        for url in "${urls[@]}"; do
+        # Loop through URLs to attempt download
+        for url in $urls; do
             # Attempting to download the script
-            wget_result="$(wget -NS "$url" -P "$path" -O MAS_AIO.cmd_update 2>&1 | grep "HTTP/" | awk '{print $2}')"
+            wget_result="$(wget -NS "$url" -P "$path" -O "$path/MAS_AIO.cmd_update" 2>&1)"
+            http_code=$(echo "$wget_result" | grep "HTTP/" | awk '{print $2}')
 
-            if [ $wget_result = 200 ]; then
+            if [ "$http_code" = "200" ]; then
                 if cmp -s "$path/MAS_AIO.cmd" "$path/MAS_AIO.cmd_update"; then
                     echo "Files are the same"
                     rm "$path/MAS_AIO.cmd_update"
                     echo "No update available $(date)" >> "$path/update.log"
-                    exit 1
+                    exit 0
                 else
                     echo "Files are different"
                     rm "$path/MAS_AIO.cmd.bak" 2>/dev/null
                     mv "$path/MAS_AIO.cmd" "$path/MAS_AIO.cmd.bak"
                     mv "$path/MAS_AIO.cmd_update" "$path/MAS_AIO.cmd"
                     echo "Updated $(date)" >> "$path/update.log"
-                    exit 1
+                    exit 0
                 fi
             else
-                echo "Failed to download from $url, HTTP status: $wget_result"
+                echo "Failed to download from $url, HTTP status: $http_code"
             fi
         done
         echo "All URLs have been tried, no successful download."
         exit 1
     else
-        echo "Cant fetch download url from remote script" >> "$path/update.log"
+        echo "Can't fetch download URL from remote script" >> "$path/update.log"
         exit 1
     fi
 else
